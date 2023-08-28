54498d83005578eedf6379ee72e6ea2647c6d632
divya <divyadc439@gmail.com>
2023-08-27 23:06:50 -0400

    4 c

--- portfolio/entrypoint.sh
+++ portfolio/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-python manage.py createsu --no-input
+python manage.py createsu 
 
 gunicorn portfolio.wsgi:application
